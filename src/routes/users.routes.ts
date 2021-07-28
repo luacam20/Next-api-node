8a29acd9e183d97e165367703bc3c9821455ebef
Luana Campos <luana.cam20@gmail.com>
2021-07-28 09:27:56 -0300

    Add project in production

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,22 @@
 import { Router } from "express"
-import { CreateUserController } from "../modules/accounts/useCases/createUsers/CreateUserController";
+import multer from "multer";
 
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvaterController";
+import { CreateUserController } from "../modules/accounts/useCases/createUsers/CreateUserController";
+import uploadConfig from "../config/upload";
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"))
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.patch(
+"/avatar", 
+uploadAvatar.single("avatar"),
+updateUserAvatarController.handle);
 
 export { usersRoutes };
